337476803ec6f38573b9613921a2b46d2551fd73
gokul <gokulap@student.tce.edu>
2021-10-31 07:17:14 +0530

    removed loop

--- auto.py
+++ auto.py
@@ -1,8 +1,6 @@
 from os import system
 from time import sleep
 
-while(1):
-  system('python geeks.py')
-  sleep(3)
-  system('python post.py')
-  sleep(43200)
+system('python geeks.py')
+sleep(2.5)
+system('python post.py')
